3d5cae8603e9da69eca096bbe8880821074a655d
Guilherme Dias Simoes <gds.simoes@gmail.com>
2024-09-10 14:25:38 -0400

    Add solution to exercise 3.11

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ morgan.token("req-body", function (req, res) {
 
 // Middleware
 app.use(cors());
+app.use(express.static("dist"));
 app.use(express.json());
 app.use(morgan(":method :url :status :res[content-length] - :response-time ms :req-body"));
 
